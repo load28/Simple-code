fbf78ac5983f95d461efae120b2096c8e7026e7b
minyeoung <fmboy07@gmail.com>
2021-03-02 20:44:32 +0900

    Revert "Initial commit"
    This reverts commit f57082529cde4d43cc1b9672f9f15f9c4c57867b.

--- rx-local-store/types.ts
+++
@@ -1,3 +0,0 @@
-export type SnapshotState<T> = Readonly<T>;
-export type UpdateStateCallback<State, NewState extends Partial<State> = Partial<State>> = (value: Readonly<State>) => NewState;
-export type MapFunction<T, R> = (value: Readonly<T>) => R;
